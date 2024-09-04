ec4be34f0b079a81eba93f37cdde4bca05a5d24d
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-09-04 16:22:32 +0530

    updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,51 +1,61 @@
 import React from 'react'
 
 const SignUp = () => {
-  return (
-    <div>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+    return (
+        <div>
+            <br></br>
+            <div className="container">
+                <div className="row">
 
-                    <div className="row g-3">
-                        <h1 align="center"><u>Register Now</u></h1>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Name</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">Phone Number</label>
-                        <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">Email Id</label>
-                        <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">Password</label>
-                        <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">Confirm Password</label>
-                        <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-success">Register</button>
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <h6>Already have an account?</h6>
-                            <a href="" className="btn btn-primary">Back to Login</a>
+                    <div class="card">
+                        <div class="card-body">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+                                <div className="row g-3">
+                                    <h1 align="center"><u>Register Now</u></h1>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Name</label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Phone Number</label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Email Id</label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Password</label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Confirm Password</label>
+                                        <input type="text" className="form-control" />
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <button className="btn btn-success">Register</button>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <h6>Already have an account?</h6>
+                                        <a href="" className="btn btn-primary">Back to Login</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+
+
+
+
                 </div>
             </div>
+
+
+
+
         </div>
-
-
-
-
-    </div>
-  )
+    )
 }
 
 export default SignUp
