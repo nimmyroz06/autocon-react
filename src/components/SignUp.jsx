f413549a57db9264e281e8372f6df395a0b1ce63
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-09-11 15:39:18 +0530

    updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './signup.css';
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="signup-background">
             <br></br>
             <div className="container">
                 <div className="row">
@@ -26,18 +27,20 @@ const SignUp = () => {
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Password</label>
-                                        <input type="text" className="form-control" />
+                                        <input type="password" className="form-control" />
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Confirm Password</label>
-                                        <input type="text" className="form-control" />
+                                        <input type="password" className="form-control" />
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                        <button className="btn btn-success">Register</button>
+                                        <br></br>
+                                        <center><button className="btn btn-success">Register</button></center>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                        <h6>Already have an account?</h6>
-                                        <a href="" className="btn btn-primary">Back to Login</a>
+                                        <center><h6>Already have an account?</h6>
+                                        <a href="" className="btn btn-primary">Back to Login</a></center>
+                                        <br></br>
                                     </div>
                                 </div>
                             </div>
