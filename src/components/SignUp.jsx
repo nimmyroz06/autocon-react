7da7e4c88dd7702dab1f70e4df3d6b1f6af86ecf
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-09-17 13:30:31 +0530

    updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,45 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signup.css';
+import axios from 'axios';
 
 const SignUp = () => {
+
+    const[input,setInput]=new useState(
+        {"name":"","phone":"","email":"","district":"","password":"","cnfpass":""}
+    )
+    const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        if (input.password==input.cnfpass) {
+            let newInput={"name":input.name,"phone":input.phone,"email":input.email,"district":input.district,"password":input.password}
+            
+
+            axios.post("",newInput).then(
+                (response)=>{
+                    console.log(response.data)
+
+                    if (response.data.status=="success") {
+                        alert("Registered Successfully")
+                    } else {
+                        alert("Email Id alraedy exist")
+                    }
+                }
+            ).catch(
+                (error)=>{
+                    console.log(error)
+                }
+            )
+            
+        } else {
+            alert("Password and Confirm Password doesn't match")
+        }
+    }
     return (
         <div className="signup-background">
-            <br></br>
             <div className="container">
                 <div className="row">
 
-                    <div class="card">
-                        <div class="card-body">
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <div className="row g-3 ">
-                                    <h1 align="center"><u>Register Now</u></h1>
+                                    <h1 align="center"><u>Create Your Account</u></h1>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Name</label>
-                                        <input type="text" className="form-control" />
+                                        <input type="text" className="form-control" name='name' value={input.name} onChange={inputHandler}/>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Phone Number</label>
-                                        <input type="text" className="form-control" />
+                                        <input type="number" className="form-control" name='phone' value={input.phone} onChange={inputHandler}/>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Email Id</label>
-                                        <input type="text" className="form-control" />
+                                        <input type="text" className="form-control" name='email' value={input.email} onChange={inputHandler}/>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">District</label>
-                                        <input type="text" className="form-control" />
+                                        <select name="district" id="" className="form-control" value={input.district} onChange={inputHandler}>
+                                            <option value="--Select--">--Select--</option>
+                                            <option value="Thiruvananthapuram">Thiruvananthapuram</option>
+                                            <option value="Kollam">Kollam</option>
+                                            <option value="Pathanamthitta">Pathanamthitta</option>
+                                            <option value="Alappuzha">Alappuzha</option>
+                                            <option value="Kottayam">Kottayam</option>
+                                            <option value="Idukki">Idukki</option>
+                                            <option value="Ernakulam">Ernakulam</option>
+                                            <option value="Thrissur">Thrissur</option>
+                                            <option value="Palakkad">Palakkad</option>
+                                            <option value="Malappuram">Malappuram</option>
+                                            <option value="Kozhikode">Kozhikode</option>
+                                            <option value="Wayanad">Wayanad</option>
+                                            <option value="Kannur">Kannur</option>
+                                            <option value="Kasargod">Kasargod</option>
+                                        </select>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Password</label>
-                                        <input type="password" className="form-control" />
+                                        <input type="password" className="form-control" name='password' value={input.password} onChange={inputHandler}/>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <label htmlFor="" className="form-label">Confirm Password</label>
-                                        <input type="password" className="form-control" />
+                                        <input type="password" className="form-control" name='cnfpass' value={input.cnfpass} onChange={inputHandler}/>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <br></br>
-                                        <center><button className="btn btn-success">Register</button></center>
+                                        <center><button onClick={readValue} className="btn btn-success">Register</button></center>
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <center><h6>Already have an account?</h6>
