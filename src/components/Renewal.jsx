5072a564392f4e8befbc9a3185f18ba393e2e5a5
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-10-19 14:24:00 +0530

    updated

--- src/components/Renewal.jsx
+++ src/components/Renewal.jsx
@@ -8,25 +8,28 @@ const Renewal = () => {
     const [message, setMessage] = useState('')
 
     const handleFileChange = (e) => {
-        const file = e.target.files[0];
+        const file = e.target.files[0]
         if (file && file.type === 'application/pdf' && file.size <= 200 * 1024) {
             setSelectedFile(file)
             setMessage('File Uploaded Successfully')
+        } else if (file && file.size > 200 * 1024) {
+            window.alert('File size should be less than 200KB.')
+            setSelectedFile(null)
         } else {
-            setMessage('Please select a valid PDF file less than 200KB.')
-            setSelectedFile(null);
+            window.alert('Please select a valid PDF file.')
+            setSelectedFile(null)
         }
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
         if (!selectedFile) {
-            setMessage('No file selected');
-            return;
+            window.alert('No file selected.')
+            return
         }
 
         const formData = new FormData();
-        formData.append('file', selectedFile);
+        formData.append('File', selectedFile);
 
         try {
             const response = await axios.post('http://localhost:5000/upload', formData, {
@@ -34,9 +37,12 @@ const Renewal = () => {
                     'Content-Type': 'multipart/form-data'
                 }
             })
-            setMessage(response.data.message)
+            if (response.status === 200) {
+                window.alert('File uploaded successfully.')
+            }
+            
         } catch (error) {
-            setMessage('File upload failed')
+            window.alert('File upload failed.')
         }
     }
 
